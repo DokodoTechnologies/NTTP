2158f597d2121b8cfa6075dd46a347ee0b9bc626
Stha07anshu <ansh091rock@gmail.com>
2025-06-23 12:15:30 +0545

    home update

--- app/index4/page.js
+++ app/index4/page.js
@@ -7,6 +7,7 @@ import Mission from "@/components/sections/home4/Mission";
 import Donation from "@/components/sections/home4/Donation";
 import Who from "@/components/sections/home4/Who";
 import Country from "@/components/sections/home4/Country";
+import LateMembersCarousel from "@/components/sections/home4/Cta";
 export default function Home() {
   return (
     <>
@@ -18,7 +19,9 @@ export default function Home() {
         <Donation />
         <Who />
         <Blog />
-        <Country />
+        <Country /> 
+        <LateMembersCarousel/>
+        
       </Layout>
     </>
   );
